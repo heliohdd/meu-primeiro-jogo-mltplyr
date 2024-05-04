d82205a8f3a026522f109c707c4457216a1301d9
Hélio Dourado <heliohdd@yahoo.com.br>
2024-05-04 12:59:30 +0000

    WebSocket real-time connection implementation

---
+++ 1st-release/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/game.js'
+import { Server } from 'socket.io'
+// import socketio from 'socket.io'
+
+
+const app = express()
+const server = http.createServer(app)
+const sockets = new Server(server)
+// const sockets = socketio(server)
+
+app.use(express.static('public'))
+
+const game = createGame()
+
+console.log(game.state)
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected on Server with id: ${playerId}`)
+
+    socket.emit('setup', game.state)
+})
+
+server.listen(3000, () => {
+    console.log(`> Server listening on port: 3000`)
+})
